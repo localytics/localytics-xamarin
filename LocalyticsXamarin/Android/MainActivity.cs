c935311342efec9550d1b89a5e26b0223f4551ca
Anand Bashyam <abasyam@localytics.com>
2018-12-24 12:41:24 -0500

    Fix Package versions to 8.0

--- LocalyticsXamarin/Android/MainActivity.cs
+++ LocalyticsXamarin/Android/MainActivity.cs
@@ -1,17 +1,8 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-
-using LocalyticsXamarin.Android;
 using LocalyticsSample.Shared;
-using Android.Gms.Common;
-using Android.Util;
 using LocalyticsXamarin.Shared;
 
 namespace LocalyticsSample.Android
@@ -19,14 +10,12 @@ namespace LocalyticsSample.Android
 
     [Activity(Label = "LocalyticsSample.Android", Icon = "@mipmap/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     [IntentFilter(new[] { Intent.ActionView },
-Categories = new[] {
-Intent.ActionView,
-Intent.CategoryDefault,
-Intent.CategoryBrowsable
-},
-DataScheme = "ampYOUR-APP-KEY"
-)
-]
+        Categories = new[] {
+        Intent.ActionView,
+        Intent.CategoryDefault,
+        Intent.CategoryBrowsable
+        },
+        DataScheme = "ampYOUR-APP-KEY")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
