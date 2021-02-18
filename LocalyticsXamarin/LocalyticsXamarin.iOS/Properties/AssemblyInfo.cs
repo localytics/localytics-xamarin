7795795b7762898c57c186f5540d75bae2475d0d
Salma Yassin <syassin@uplandsoftware.com>
2021-02-18 23:58:57 +0200

    CXMM-1863 - Migrated from Android to AndroidX, added *.log in gitignore, and commented AssemblyInfo for duplication error

--- LocalyticsXamarin/LocalyticsXamarin.iOS/Properties/AssemblyInfo.cs
+++ LocalyticsXamarin/LocalyticsXamarin.iOS/Properties/AssemblyInfo.cs
@@ -11,21 +11,21 @@ using Foundation;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-
-[assembly: AssemblyTitle ("LocalyticsXamarin.iOS")]
-[assembly: AssemblyDescription ("Localytics℠ for Xamarin.iOS")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Localytics")]
-[assembly: AssemblyProduct ("Localytics℠ for Xamarin.iOS")]
-[assembly: AssemblyCopyright ("© 2008-2020 Localytics℠")]
-[assembly: AssemblyTrademark ("Localytics℠")]
-[assembly: AssemblyCulture ("")]
+//Commented 
+//[assembly: AssemblyTitle ("LocalyticsXamarin.iOS")]
+//[assembly: AssemblyDescription ("Localytics℠ for Xamarin.iOS")]
+//[assembly: AssemblyConfiguration ("")]
+//[assembly: AssemblyCompany ("Localytics")]
+//[assembly: AssemblyProduct ("Localytics℠ for Xamarin.iOS")]
+//[assembly: AssemblyCopyright ("© 2008-2020 Localytics℠")]
+//[assembly: AssemblyTrademark ("Localytics℠")]
+//[assembly: AssemblyCulture ("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.0.0.*")]
+//[assembly: AssemblyVersion ("0.0.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
